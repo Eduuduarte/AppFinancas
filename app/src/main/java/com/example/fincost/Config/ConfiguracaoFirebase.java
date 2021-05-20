524d92d67f25a2b63d5e9ad1e0e64114aca2abec
Eduardo Duarte <eduardoievolucao@gmail.com>
2021-05-19 22:09:15 -0300

    Alteração feita

--- app/src/main/java/com/example/fincost/Config/ConfiguracaoFirebase.java
+++ app/src/main/java/com/example/fincost/Config/ConfiguracaoFirebase.java
@@ -1,4 +1,28 @@
 package com.example.fincost.Config;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class ConfiguracaoFirebase {
+    private static DatabaseReference firebaseReference;
+    private static FirebaseAuth autenticacao;
+
+    public static DatabaseReference getFirebase(){
+
+        if(firebaseReference == null){
+            firebaseReference = FirebaseDatabase.getInstance().getReference();
+        }
+        
+        return firebaseReference;
+    }
+
+    public static FirebaseAuth getFirebaseAutenticacao(){
+
+        if(autenticacao == null){
+            autenticacao = FirebaseAuth.getInstance();
+        }
+        return autenticacao;
+    }
+
 }
